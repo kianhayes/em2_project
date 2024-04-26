59342297a84ded07540ee257ac0e1cfa4e4fcc67
StevenFlores <sflores021403@gmail.com>
2024-04-26 13:06:31 -0500

    4/26

--- 7.13/7.13_test4.py
+++ 7.13/7.13_test4.py
@@ -3,31 +3,36 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib.cm import ScalarMappable
 
-k = 1
+k = 1e-7
 a = 1
 
 def b_field(y, t, k):
     B = k * y**3 * t**2
     return B
 
-x = np.linspace(0, 10, num=100)
-y = np.linspace(0, 10, num=100)
+x = np.linspace(-10, 10, num=100)
+y = np.linspace(-10, 10, num=100)
 z = np.linspace(0, 10, num=100)
-t = np.linspace(0, 100, 10)  # Selecting a single time step for visualization
-
+a = np.linspace(-10,10, num=100)
+ts = np.linspace(0, 100, 10)  # Selecting a single time step for visualization
+fluxs = []
 X, Y = np.meshgrid(x, y)
-for t in t:
-    flux = (1/4)*k*t**2*a**5
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.plot_surface(X, Y, flux, cmap='magma',linewidth=0,antialiased=False, vmin=0, vmax=100)
-    #labeling
-    ax.set_xlabel('X', labelpad=20)
-    ax.set_ylabel('Y', labelpad=20)
-    ax.set_zlabel('Z', labelpad=20)
-    ax.set_title("3D Scalar field")
-    plt.show()
 
+
+for t in ts:
+    flux = (1/4)*k*t**2*a**5
+    fluxs.append(flux)
+
+#ax = fig.add_subplot(projection='3d')
+plt.plot(ts, flux)
+'''
+ax.set_xlabel('X', labelpad=20)
+ax.set_ylabel('Y', labelpad=20)
+ax.set_zlabel('Z', labelpad=20)
+ax.set_title("3D Scalar field")
+
+'''
+plt.show()
 # for t in t:
 #     # Calculate magnetic field magnitude at each point
 #     B = b_field(Y, t, k)
@@ -56,12 +61,12 @@ for t in t:
 
 
 # Calculate sizes of points based on the magnitude of the magnetic field
-# sizes = 10 * np.abs(B)
+sizes = 10 * np.abs(B)
 
-# Flatten the arrays for plotting
-# X_flat = X.flatten()
-# Y_flat = Y.flatten()
-# Z_flat = Z.flatten()
+#Flatten the arrays for plotting
+X_flat = X.flatten()
+Y_flat = Y.flatten()
+Z_flat = Z.flatten()
 
 '''
 #fig = plt.figure(figsize=(10, 8))
